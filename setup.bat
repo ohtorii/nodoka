a988ee46861c9a4fee0051f32fd134e541e23180
ohtorii <ikeuchi.h9@gmail.com>
2018-09-27 21:08:10 +0900

    add file.

---
+++ setup.bat
@@ -0,0 +1,18 @@
+@echo off
+REM
+REM WindowsÇÃä¬ã´ê›íË
+REM
+
+setlocal
+set NODOKA=C:\Program Files\nodoka
+
+call :proc personal.nodoka
+call :proc hidemaru.nodoka
+exit /b 0
+
+
+:proc
+echo %1
+del /F "%NODOKA%\%1"
+mklink /H "%NODOKA%\%1" "%~dp0%1"
+exit /b 0
